0af29b0bba3afe5bbae71ab90456beaf8622abe1
olli somero <olli.somero@varjo.design>
2020-12-11 11:38:12 +0200

    ksdafjsa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import Logo from './assets/luihunen.png';
+import {DESKTOP, MAX_WIDTH, MOBILE} from "./constants";
+
 function App() {
+    const [screenMode, setScreenMode] = useState(undefined);
+
+    useEffect(() => {
+        getScreenMode();
+        window.addEventListener('resize', () => getScreenMode());
+    }, [])
+
+    const getScreenMode = () => {
+        setScreenMode(window.innerWidth < MAX_WIDTH ? MOBILE : DESKTOP);
+    }
   return (
-    <div>
-      Luihunen
+    <div style={{display: 'flex', justifyContent: 'center', aligItems: 'center', minHeight: '100vh'}}>
+        <div>
+            <div>
+                <img
+                    style={{width: screenMode === DESKTOP ? 400 : 300, height: screenMode === DESKTOP ? 400 : 300}}
+                    src={Logo}/>
+            </div>
+            <div>
+                Rakennamme Luihuista verkkokauppaa.
+            </div>
+            <div style={{paddingTop: 16}}>
+                Aukioloajat:
+            </div>
+            <div>
+                Osoite
+            </div>
+            <div>
+                Puhelinnumero
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const DESKTOP = 'DESKTOP';
+export const MOBILE = 'MOBILE';
+export const MAX_WIDTH = 900;
